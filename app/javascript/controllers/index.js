06678d23d4622e237fd90fe08f9a179e5ae6726a
danglduy <38782010+danglduy@users.noreply.github.com>
2022-08-11 16:56:05 +0700

    Display reaction summary while hovering

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import ReactionSummaryController from "./reaction_summary_controller"
+application.register("reaction-summary", ReactionSummaryController)
